b5e35322ea36ed3f6efd19194e991f69f94e0121
Court711 <elmc1_16@uni.worc.ac.uk>
2017-10-16 14:15:19 +0100

    test
    testtestetestetesstest

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,28 @@
-var paused_count =0;
-var resumed_count = 0;
-var launched_count = 0;
-//event defined
-document.addEventListener("deviceready", onDeviceReady, false);
-		
-	
-function updateDisplay() {
-	$("#launched").text("Application Launched: " + launched_count);
-	$("#resumed").text("Application Paused: " + paused_count);
-	$("#paused").text("Application Resumed: " + resumed_count);
-}
+var key = “age”;
+var value = “20”;
+var key2 = “bradage”;
+var value2 = “21”;
+var key3 = “zaibage”;
+var value3 = “19”;
+var key4 = “kateage”;
+var value4 = “18”;
+var key5 = “jasonage”;
+var value5 = “26”;
+
+window.localStorage.setItem( key, value );
 
 
-// device APIs are available
-//
-    function onDeviceReady() {
-	//event defined
-	document.addEventListener("resume", onResume, false);
-	document.addEventListener("pause", onPause, false);
-	
-	launched_count++;
-	updateDisplay();
-	    
-	alert("device ready");
-    }
+window.localStorage.setItem( key2, value2 );
 
-    // pause lifecycle event triggered
-    function onPause() {
-	
-	paused_count++;
-	updateDisplay();
-	    
-	alert("pause");
-    }
-	
-    // resume lifecycle event triggered
-    function onResume() {
-		
-	resumed_count++;
-	updateDisplay();
-	    
-	alert("resume");
-    }
+
+window.localStorage.setItem( key3, value3 );
+
+
+window.localStorage.setItem( key4, value4 );
+
+
+window.localStorage.setItem( key5, value5 );
+
+var test = window.localStorage.getItem(bradage);
+
+alert($test);
