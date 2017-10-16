c4330e8c93671c2767660b9df8084741e76344e9
Court711 <elmc1_16@uni.worc.ac.uk>
2017-10-16 15:02:52 +0100

    test4

--- www/js/index.js
+++ www/js/index.js
@@ -15,44 +15,44 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function updateDisplay() {
     
-    var key = “age”;
-    var value = “20”;
+    var key = "age";
+    var value = "20";
     window.localStorage.setItem( key, value );
-    var key = “bradage”;
-    var value = “21”;
+    key = "bradage";
+    value = "21";
     window.localStorage.setItem( key, value );
-    var key = “zaibage”;
-    var value = “19”;
+    key = "zaibage";
+    value = "19";
     window.localStorage.setItem( key, value );
-    var key = “kateage”;
-    var value = “18”;
+    key = "kateage";
+    value = "18";
     window.localStorage.setItem( key, value );
-    var key = “jasonage”;
-    var value = “26”;
+    key = "jasonage";
+    value = "26";
     window.localStorage.setItem( key, value );
     
 	$("#launched").text("Application Launched: " + launched_count);
 	$("#resumed").text("Application Paused: " + paused_count);
 	$("#paused").text("Application Resumed: " + resumed_count);
     
-    var key = “age”;
-    var value = window.localStorage.getItem(key);
+    key = "age";
+    value = window.localStorage.getItem(key);
     $("#d1").text(key + "is: " + value);
     
-    var key = “bradage”;
-    var value = window.localStorage.getItem(key);
+    key = "bradage";
+    value = window.localStorage.getItem(key);
     $("#d2").text(key + "is: " + value);
     
-    var key = “zaibage”;
-    var value = window.localStorage.getItem(key);
+    key = "zaibage";
+    value = window.localStorage.getItem(key);
     $("#d3").text(key + "is: " + value);
     
-    var key = “kateage”;
-    var value = window.localStorage.getItem(key);
+    key = "kateage";
+    value = window.localStorage.getItem(key);
     $("#d4").text(key + "is: " + value);
     
-    var key = “jasonage”;
-    var value = window.localStorage.getItem(key);
+    key = "jasonage";
+    value = window.localStorage.getItem(key);
     $("#d5").text(key + "is: " + value);
     
     //var key = “pen”;
